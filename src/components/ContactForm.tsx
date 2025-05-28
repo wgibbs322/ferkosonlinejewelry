3d54a496fafbe1ce10c59b11f9632c58477a064b
wgibbs322 <wgibbs322@gmail.com>
2025-05-28 10:56:36 -0700

    m

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -14,7 +14,7 @@ const ContactForm: React.FC = () => {
     setStatus('loading');
 
     try {
-      const response = await fetch('http://localhost:3000/api/contact', {
+      const response = await fetch('https://ferkosonlinebackend.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
